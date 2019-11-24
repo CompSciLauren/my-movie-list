c62598e8838af01941dc32f9e9bdc5fb06a4a4e3
Lauren Stephenson <compscilauren@gmail.com>
2019-11-24 11:29:33 -0600

    Replace settings template with real content

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,10 +1,41 @@
 import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function SettingsScreen() {
-  return <ExpoConfigView />;
+  return (
+    <View style={styles.container}>
+      <ScrollView
+        style={styles.container}
+        contentContainerStyle={styles.contentContainer}
+      >
+        <View style={styles.welcomeContainer}>
+          <Text style={styles.welcomeText}>Settings Screen</Text>
+        </View>
+      </ScrollView>
+    </View>
+  );
 }
 
 SettingsScreen.navigationOptions = {
   title: 'Settings',
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  contentContainer: {
+    paddingTop: 30,
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  welcomeText: {
+    fontSize: 18,
+    color: 'rgba(70,70,70, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+});
